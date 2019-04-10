9ad474202cc7284812f8049177d9a86641c1b86b
Romulogss <romulo.gabss@gmail.com>
2019-04-10 19:29:03 -0300

    Commit inicial

---
+++ Aulas_P.O.O_BSI_2019.1/src/Aula_10_04_2019/HelloWord.java
@@ -0,0 +1,11 @@
+package Aula_10_04_2019;
+
+/**
+ *
+ * @author Rômulo
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo, caralho!");
+    }
+}
