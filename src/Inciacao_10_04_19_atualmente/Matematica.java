c7128817b0ccf716f4c60fb190e45401862717a2
Romulogss <46789931+Romulogss@users.noreply.github.com>
2019-04-28 16:59:03 +0000

    Update Matematica.java

--- src/Inciacao_10_04_19_atualmente/Matematica.java
+++ src/Inciacao_10_04_19_atualmente/Matematica.java
@@ -11,14 +11,14 @@ public class Matematica {
     }
     
     public static double subtrair(double valor, double valor2) {
-        return valor + valor2;
+        return valor - valor2;
     }
     
     public static double dividir(double valor, double valor2) {
-        return valor + valor2;
+        return valor / valor2;
     }
     
     public static double multiplicar(double valor, double valor2) {
-        return valor + valor2;
+        return valor * valor2;
     }
 }
