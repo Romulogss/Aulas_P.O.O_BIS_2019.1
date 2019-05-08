ce3b37614bdcee822f1bb497b958caa3bd421ef1
Romulogss <romulo.gabss@gmail.com>
2019-05-08 14:43:25 -0300

    Exercicio 02

--- src/exerciciosMonitoria/FrasePalavraLetra.java
+++ src/exerciciosMonitoria/FrasePalavraLetra.java
@@ -7,35 +7,44 @@ import java.util.Scanner;
  * @author Rômulo
  */
 public class FrasePalavraLetra {
-    public static boolean letraEspecial(char letra){
-        if(letra == 'á' && letra == 'à' && letra == 'ã' && letra == 'â'){
-            return true;
-        } else if(letra == 'ó' && letra == 'ô' && letra == 'õ'){
-            return true;
-        } else if (letra == 'é' && letra == 'ê') {
-            
-        } else {
+
+    public static boolean isVoagal(char letra) {
+        switch (letra) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                return true;
+
         }
         return false;
     }
-    public static void main(String[] args){
-    Scanner scanner = new Scanner(System.in);
-    String frase;
-    int palavras = 0, letras = 0, vogais = 0, concoantes = 0;
 
-    System.out.print("Informe uma frase: ");
-    frase = scanner.nextLine();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String frase;
+        int palavras = 0, letras = 0, vogais = 0, concoantes = 0;
 
-    for(int i = 0; frase.length() > i; i++){
-      char letraAtual = frase.charAt(i);
-      if(Character.isAlphabetic(letraAtual)){
-        letras ++;
-      } else if(letraAtual == ' '){
-        palavras++;
-      }
+        System.out.print("Informe uma frase: ");
+        frase = scanner.nextLine();
+
+        for (int i = 0; frase.length() > i; i++) {
+            char letraAtual = frase.charAt(i);
+            if (Character.isAlphabetic(letraAtual)) {
+                letras++;
+            } else if (letraAtual == ' ') {
+                palavras++;
+            }
+            if (isVoagal(letraAtual)) {
+                vogais++;
+            }
+        }
+        concoantes = letras - vogais;
+        System.out.println("A frase: \"" + frase + "\", tem:");
+        System.out.println(letras + " letras.");
+        System.out.println((palavras + 1) + " palavras.");
+        System.out.println(concoantes + "conçoantes.");
+        System.out.println(vogais + "vogais.");
     }
-
-    System.out.println(letras);
-    System.out.println(palavras+1);
-  }
 }
